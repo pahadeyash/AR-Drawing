ae1d01997c3eacb0ce17f32363eea8e7a7bd325b
Rayan Slim <rayanslim@Rayans-MacBook-Pro.local>
2017-08-04 15:41:06 +0300

    2nd video

--- AR Drawing/ViewController.swift
+++ AR Drawing/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import ARKit
+class ViewController: UIViewController, ARSCNViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var SceneView: ARSCNView!
+    let configuration = ARWorldTrackingSessionConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.SceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.SceneView.showsStatistics = true
+        self.SceneView.session.run(configuration)
+        self.SceneView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +25,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        print("rendering")
+        guard let pointOfView = self.SceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let position = orientation + location
+        print(orientation.x, orientation.y, orientation.z)
+    }
 
 
 }
+
+func + (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
+
 
