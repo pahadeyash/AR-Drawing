b68e08f4bed86e2b4bd8c445eb98423d705c8cf6
Yash Pahade <yash.aries.6@gmail.com>
2018-04-17 20:01:24 -0700

    first commit

--- AR Drawing/ViewController.swift
+++ AR Drawing/ViewController.swift
@@ -2,16 +2,17 @@
 //  ViewController.swift
 //  AR Drawing
 //
-//  Created by Rayan Slim on 2017-08-04.
-//  Copyright © 2017 Rayan Slim. All rights reserved.
+//  Created by Yash Pahade on 2018-17-04.
+//  Copyright © 2018 Yash Pahade. All rights reserved.
 //
 
 import UIKit
 import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var draw: UIButton!
     @IBOutlet weak var SceneView: ARSCNView!
-    let configuration = ARWorldTrackingSessionConfiguration()
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.SceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
@@ -27,13 +28,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
-        print("rendering")
+        //print("rendering")
         guard let pointOfView = self.SceneView.pointOfView else {return}
         let transform = pointOfView.transform
         let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
         let location = SCNVector3(transform.m41,transform.m42,transform.m43)
         let position = orientation + location
-        print(orientation.x, orientation.y, orientation.z)
+        //print(orientation.x, orientation.y, orientation.z)
+        DispatchQueue.main.async {
+            if self.draw.isHighlighted{
+                //print("Draw button is highlighted")
+                let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
+                sphereNode.position = position
+                self.SceneView.scene.rootNode.addChildNode(sphereNode)
+                sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+            } else {
+                let pointer = SCNNode(geometry: SCNSphere(radius: 0.02))
+                pointer.name = "pointer"
+                pointer.position = position
+                self.SceneView.scene.rootNode.enumerateHierarchy({ (node, _) in
+                    if node.name == "pointer"{
+                        node.removeFromParentNode()
+                    }
+                })
+                self.SceneView.scene.rootNode.addChildNode(pointer)
+                pointer.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+                
+            }
+        }
+
     }
 
 
